248d0aaea07628b0381b0cd54b4f422675eab4e7
Jaspreet singh <jaspreet.singh70077@gmail.com>
2023-07-09 13:45:59 +0530

    variable added

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "aws_region" {
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-065deacbcaac64cf2"
+   default     = "ami-0f5ee92e2d63afc18"
 }
 
 variable "instance_type" {
