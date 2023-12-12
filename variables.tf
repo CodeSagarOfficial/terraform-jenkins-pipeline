c6a4a0ba72f76b30f99f8739ac48b2c2b42d2d53
collinsako <collinsako.ca23@gmail.com>
2023-12-11 21:38:56 -0500

    update

--- variables.tf
+++ variables.tf
@@ -13,14 +13,14 @@ variable "aws_secret_key" {
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+   default     = "ami-0fc5d935ebf8bc3bc"
 }
 
 variable "instance_type" {
